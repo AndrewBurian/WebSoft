270c5b7f8a4f3cf23a7088638f01c68662b0bf30
Cholisky <Cholisky@gmail.com>
2014-02-16 07:31:13 -0800

    User management: 	now able to add 	validation working 	errors (if any) displayed 	code needs cleanup

--- application/controllers/usermtce.php
+++ application/controllers/usermtce.php
@@ -61,28 +61,35 @@ class Usermtce extends Application {
             'lastvisited');
 
         // either create or retrieve the relevant user record
-        if ($id == null || $id == 'new')
+        if ($id == null || $id == 'new') {
+            $id = null;
             $user = $this->users->create();
-        else
+        } else {
             $user = $this->users->get($id);
+        }
 
         // over-ride the user record fields with submitted values
         fieldExtract($_POST, $user, $user_fields);
 
         // validate the user fields
-        if ($_POST['id'] == 'new' || empty($_POST['id']))
+        if (empty($_POST['id'])) {
             $this->data['errors'][] = 'You need to specify a userid';
-        echo 
-        if ($id == null && $this->users->exists($_POST['id']))
+        }
+         if ($_POST['id'] == 'new') {
+            $this->data['errors'][] = 'new is not a valid userid';
+        }
+        if ($id == null && $this->users->exists($_POST['id'])) {
             $this->data['errors'][] = 'That userid is already used';
-        if (strlen($user->name) < 1)
+        }
+        if (strlen($user->name) < 1) {
             $this->data['errors'][] = 'You need a user name';
-        if (strlen($user->email) < 1)
+        }
+        if (strlen($user->email) < 1) {
             $this->data['errors'][] = 'You need an email address';
-        if (!strpos($user->email, '@'))
+        }
+        if (!strpos($user->email, '@')) {
             $this->data['errors'][] = 'The email address is missing the domain';
-        if ($id == null && empty($user->password))
-            $this->data['errors'][] = 'You must specify a password';
+        }
 
         // if errors, redisplay the form
         if (count($this->data['errors']) > 0) {
@@ -90,30 +97,30 @@ class Usermtce extends Application {
             $this->data = array_merge($this->data, (array) $user);
             $this->data['pagebody'] = 'useredit';
             $this->render();
-            exit;
+             foreach ($this->data['errors'] as $booboo)
+            echo '<p>' . $booboo . '</p>';
+             exit;
         }
-
         // handle the password specially, as it needs to be encrypted
         $new_password = $_POST['password'];
         if (!empty($new_password)) {
             $new_password = md5($new_password);
-          //  if ($new_password != $user['password'])
-                $user->password = $new_password;
+            //  if ($new_password != $user['password'])
+            $user->password = $new_password;
         }
-
+        if ($id == null && empty($user->password)) {
+            $this->data['errors'][] = 'You must specify a password';
+        }
         // either add or update the user record, as appropriate
         if ($id == null) {
             $user->id = $_POST['id'];
-            //$this->users->add($user);
             $this->users->add($user);
-        }
-        else
+        } else {
             $this->users->update($user);
-
+        }
         // redisplay the list of users
         redirect('/usermtce');
     }
-
     // Delete a user
     function delete($id) {
         $this->users->delete($id);
