0bab5bbf806ee6a2263971defe7b80413ecc47ab
AndrewBurian <andrew@burian.ca>
2014-02-19 12:34:56 -0800

    Started to move login bar to view template

---
+++ application/views/login/_loginBar.php
@@ -0,0 +1,3 @@
+<a href="/login">
+    <input type="button" value="Log In"/>
+</a>
---
+++ application/views/login/_userBar.php
@@ -0,0 +1,2 @@
+{user_name} | <a href="/logout">Logout</a>
+<!--<a href="/logout"><input type="button" value="Log Out"/></a>-->
