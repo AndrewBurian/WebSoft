37fd2aef6e529a5ee22002d93dcd18026f206d5a
Cholisky <Cholisky@gmail.com>
2014-02-23 00:51:10 -0800

    Changed scripts/all_tables.sql - added to images and posts tables

--- scripts/all_tables.sql
+++ scripts/all_tables.sql
@@ -16,6 +16,8 @@ CREATE TABLE IF NOT EXISTS `images`
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
+
 CREATE TABLE IF NOT EXISTS `users`
 (
     `id`        INTEGER(18)     NOT NULL    AUTO_INCREMENT,
@@ -59,5 +61,12 @@ INSERT INTO `users` (`id`, `name`, `password`, `role`, `email`, `status`, `pic`)
 (1, 'ABurian', md5('AB'), 'admin', 'a@b.com', 'A', 0),
 (2, 'CHolisky', md5('CH'), 'user', 'c@h.com', 'A', 0);
 
-INSERT INTO `images` (`id`, `filename`, `author`, `date`, `caption`, `licence`, `thumbnail`, `file`) VALUES 
-(0, 'default.png', 'default', CURRENT_TIMESTAMP, 'No image', NULL, NULL, NULL);
+INSERT INTO `images` (`id`, `filename`, `author`, `date`, `caption`, `licence`, `thumbnail`, `file`) VALUES
+(1, 'what', 'Who knows', '2014.09.3', 'A flower', 'CCL', 'img01.jpg', 'img01.jpg'),
+(2, 'img02.jpg', 'Who knows', '2014.09.2', 'A balloon', 'CCL', 'img01.jpg', 'img01.jpg'),
+(3, 'img03.jpg', 'Who knows', '2014.09.1', 'More than one balloon', 'CCL', 'img01.jpg', 'img01.jpg');
+
+INSERT INTO `posts` (`id`, `user`, `title`, `slug`, `story`, `updated`, `created`, `pic`) VALUES
+(1,'1', 'The first pub', 'The waitresses were pretty but kinda slow.', '<p>Seriously, folks.  Why do so many places hire pretty girls that can pass light between their ears?</p>\r\n<p>If I want overpriced beer and pretty girls I don''t go to a pub, I go to Sammy J Peppers.</p>\r\n<p>Right now all I''m doing is filling space.</p>',  '2014.09.3',  '2014.09.3', '1'),
+(2,'2', 'The second pub','This one had decent food.', '<p>So we decided to eat at this one to help soak up some alchohol.  The plan was that we remember what happened this time.</p>\r\n<p>We ate and I recall that we made exclaimations over the food but we also sampled from alll 278 taps.</p>\r\n<p>Now I''m just trying to fill some space so that this looks a little more like a real post.  Yay for creative writing!</p>','2014.09.2',  '2014.09.2','2'),
+(3,'2', 'Another pub',   'Another pub.', '<p>There was beer to be had and we went to drink it.</p>\r\n<p>It was good but we didn''t write anything down.  We forgot in the joy of the sweet amber elixer.</p>\r\n<p>If you want to enjoy forgetfulness, try this pub.</p>','2014.09.1',  '2014.09.1', '3');                                                                  
