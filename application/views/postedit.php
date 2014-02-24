0b9dfdb6fd89326ed6ba74f43d0c04d3232cd91b
AndrewBurian <andrew@burian.ca>
2014-02-23 17:10:54 -0800

    All but the RTF working as expected.

--- application/views/postedit.php
+++ application/views/postedit.php
@@ -1,28 +1,5 @@
-<?php
-if (!defined('APPPATH'))
-    exit('No direct script access allowed');
-/**
- * views/postedit.php
- *
- * Manage a posting's contents
- *
- * @author		JLP
- * ------------------------------------------------------------------------
- */
-if (isset($errors) && count($errors) > 0) {
-    ?>
-    <div class="alert alert-error">
-        <p><strong></strong></p>
-        <?php
-        foreach ($errors as $booboo)
-            echo '<p>' . $booboo . '</p>';
-        ?>
-    </div>
-<?php }
-?>
-
-
 <form action="/postmtce/submit/{id}" method="post">
+    {field_errors}
     {field_pic}<br/>
     {field_title}<br/>
     {field_date}<br/>
