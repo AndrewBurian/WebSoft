17b9a9e1902c713b3e7602e97fb9b826255e3472
Cholisky <Cholisky@gmail.com>
2014-03-16 14:22:19 -0700

    added info to site_values model

--- application/models/site_info.php
+++ application/models/site_info.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,39 +8,57 @@
 
 class Site_info extends CI_model {
 
-
     // Constructor
     function __construct() {
         parent::__construct();
+        $this->setTable('rpc_data');
     }
-    
-    
-    function getName(){
-        return "Vancouver Pub Reviews";
+
+    function getName() {
+        $result = "";
+        $result .= $this->get('name');
+        return $result;
+        // return "Greater Vancouver Pub Reviews";
     }
-    
-    function getCode(){
-        return "O07";
+
+    function getCode() {
+        $result = "";
+        $result .= $this->get('code');
+        return $result;
+        //return "O07";
     }
-    
-    function getLink(){
+
+    function getLink() {
         return $_SERVER['HTTP_HOST'];
     }
-    
-    function getPlug(){
-        return "Casual reviews of our favorite watering holes.";
+
+    function getPlug() {
+        $result = "";
+        $result .= $this->get('plug');
+        return $result;
+        // return "Casual reviews of our favorite watering holes.";
     }
-    
-    function setName(){
-        throw new Exception("Not yet supported");
+
+    function setName($name) {
+
+        $siteName = $this->get_array('name');
+        $siteName['name'] = $name;
+        $this->update($siteName);
+        // throw new Exception("Not yet supported");
     }
-    
-    function setCode(){
-        throw new Exception("Not yet supported");
+
+    function setCode($code) {
+        $siteCode = $this->get_array('code');
+        $siteCode['code'] = $code;
+        $this->update($siteCode);
+        //throw new Exception("Not yet supported");
     }
-    
-    function setPlug(){
-        throw new Exception("Not yet supported");
+
+    function setPlug($plug) {
+        $sitePlug = $this->get_array('plug');
+        $sitePlug['plug'] = $plug;
+        $this->update($sitePlug);
+        // throw new Exception("Not yet supported");
     }
-    
+
 }
