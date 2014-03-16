81bc8d37ba5b58809884f6b3ad784f2edbc8c413
Cholisky <Cholisky@gmail.com>
2014-03-16 15:09:29 -0700

    site_info model now actually works

--- application/models/site_info.php
+++ application/models/site_info.php
@@ -11,21 +11,20 @@ class Site_info extends _Mymodel {
     // Constructor
     function __construct() {
         parent::__construct();
-        $this->setTable('rpc_data');
+        $this->setTable('rpc_data', 'varid');
     }
 
     function getName() {
-        $result = "";
-        $result .= $this->get('name');
-        return $result;
+
+        $siteName = $this->get_array('0');
+        return $siteName['name'];
         // return "Greater Vancouver Pub Reviews";
     }
 
     function getCode() {
-        $result = "";
-        $result .= $this->get('code');
-        return $result;
-        //return "O07";
+        $siteCode = $this->get_array('0');
+        return $siteCode['code'];
+        // return "O07";
     }
 
     function getLink() {
@@ -33,29 +32,28 @@ class Site_info extends _Mymodel {
     }
 
     function getPlug() {
-        $result = "";
-        $result .= $this->get('plug');
-        return $result;
-        // return "Casual reviews of our favorite watering holes.";
+        $sitePlug = $this->get_array('0');
+        return $sitePlug['plug'];
+        //  return "Casual reviews of our favorite watering holes.";
     }
 
     function setName($name) {
 
-        $siteName = $this->get_array('name');
+        $siteName = $this->get_array('0');
         $siteName['name'] = $name;
         $this->update($siteName);
         // throw new Exception("Not yet supported");
     }
 
     function setCode($code) {
-        $siteCode = $this->get_array('code');
+        $siteCode = $this->get_array('0');
         $siteCode['code'] = $code;
         $this->update($siteCode);
         //throw new Exception("Not yet supported");
     }
 
     function setPlug($plug) {
-        $sitePlug = $this->get_array('plug');
+        $sitePlug = $this->get_array('0');
         $sitePlug['plug'] = $plug;
         $this->update($sitePlug);
         // throw new Exception("Not yet supported");
